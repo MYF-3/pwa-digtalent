4caa7edbd2e0724835666a1ada8e191064bb717f
MuhammadYusufFirstra <yusuf.firstra@gmail.com>
2020-10-23 19:38:45 +0700

    commit

--- js/api.js
+++ js/api.js
@@ -5,43 +5,42 @@ var standing_url = `${base_url}competitions/${league_id}/standings`;
 var team_url = `${base_url}teams/`;
 
 var fetchApi = url => {
-  return fetch(url, 
-    { 
-      mode : 'cors',
-      headers: {'X-Auth-Token': token }
+    return fetch(url, {
+        mode: 'cors',
+        headers: { 'X-Auth-Token': token }
     });
 }
 
 // Blok kode yang akan di panggil jika fetch berhasil
 function status(response) {
-  if (response.status !== 200) {
-    console.log("Error : " + response.status);
-    // Method reject() akan membuat blok catch terpanggil
-    return Promise.reject(new Error(response.statusText));
-  } else {
-    // Mengubah suatu objek menjadi Promise agar bisa "di-then-kan"
-    return Promise.resolve(response);
-  }
+    if (response.status !== 200) {
+        console.log("Error : " + response.status);
+        // Method reject() akan membuat blok catch terpanggil
+        return Promise.reject(new Error(response.statusText));
+    } else {
+        // Mengubah suatu objek menjadi Promise agar bisa "di-then-kan"
+        return Promise.resolve(response);
+    }
 }
 
 // Blok kode untuk memparsing json menjadi array JavaScript
 function json(response) {
-  return response.json();
+    return response.json();
 }
 
 // Blok kode untuk meng-handle kesalahan di blok catch
 function error(error) {
-  // Parameter error berasal dari Promise.reject()
-  console.log("Error : " + error);
+    // Parameter error berasal dari Promise.reject()
+    console.log("Error : " + error);
 }
 
 // Blok kode untuk melakukan request data json
 function getStandings() {
     if ("caches" in window) {
-      caches.match(standing_url).then(function(response) {
-        if (response) {
-          response.json().then(function(data) {
-              var standingsHTML =  `
+        caches.match(standing_url).then(function(response) {
+            if (response) {
+                response.json().then(function(data) {
+                    var standingsHTML = `
                   <table style="font-size:14px;" class="responsive-table">
                     <thead>
                       <tr>
@@ -60,8 +59,8 @@ function getStandings() {
                     </thead>
                     <tbody>
               `;
-              data.standings["0"].table.forEach(function(item) {
-                standingsHTML += `
+                    data.standings["0"].table.forEach(function(item) {
+                        standingsHTML += `
                         <tr>
                           <td>${item.position}</td>
                           <td><a href="./team.html?id=${item.team.id}"><img style="width:20px;" src="${item.team.crestUrl}"></a></td>
@@ -76,20 +75,20 @@ function getStandings() {
                           <td>${item.points}</td>
                         </tr>
                 `;
-            });
-            standingsHTML += `</tbody>
+                    });
+                    standingsHTML += `</tbody>
                     </table>`;
-            document.getElementById("standings").innerHTML = standingsHTML;
-          });
-        }
-      });
+                    document.getElementById("standings").innerHTML = standingsHTML;
+                });
+            }
+        });
     }
     fetchApi(standing_url)
-      .then(status)
-      .then(json)
-      .then(function(data) {
-        console.log(data);
-        var standingsHTML =  `
+        .then(status)
+        .then(json)
+        .then(function(data) {
+            console.log(data);
+            var standingsHTML = `
                 <table style="font-size:14px;" class="responsive-table">
                   <thead>
                     <tr>
@@ -108,8 +107,8 @@ function getStandings() {
                   </thead>
                   <tbody>
             `;
-          data.standings["0"].table.forEach(function(item) {
-            standingsHTML += `
+            data.standings["0"].table.forEach(function(item) {
+                standingsHTML += `
                     <tr>
                       <td>${item.position}</td>
                       <td><a href="./team.html?id=${item.team.id}"><img style="width:20px;" src="${item.team.crestUrl}"></a></td>
@@ -124,23 +123,24 @@ function getStandings() {
                       <td>${item.points}</td>
                     </tr>
             `;
-        });
-        standingsHTML += `</tbody>
+            });
+            standingsHTML += `</tbody>
                 </table>`;
-        document.getElementById("standings").innerHTML = standingsHTML;
-      })
-      .catch(error);
+            document.getElementById("standings").innerHTML = standingsHTML;
+        })
+        .catch(error);
 }
 
 function getTeamById() {
-    var urlParams = new URLSearchParams(window.location.search);
-    var idParam = urlParams.get("id");
-    var team_id_url = `${base_url}teams/${idParam}`;
-    if ("caches" in window) {
-      caches.match(team_id_url).then(function(response) {
-        if (response) {
-          response.json().then(function(data) {
-            var teamHTML = `
+    return new Promise(function(resolve, reject) {
+        var urlParams = new URLSearchParams(window.location.search);
+        var idParam = urlParams.get("id");
+        var team_id_url = `${base_url}teams/${idParam}`;
+        if ("caches" in window) {
+            caches.match(team_id_url).then(function(response) {
+                if (response) {
+                    response.json().then(function(data) {
+                        var teamHTML = `
             <div class="row">
               <h4 class="light center grey-text text-darken-3" style="font-size:40px; font-weight:bold;"><img style="width:90px;" src="${data.crestUrl}"> <br>${data.name}</h4>
                     <p align="center">Nickname : ${data.shortName}<br>Address : ${data.address}<br>Founded : ${data.founded}<br>Club Colors : ${data.clubColors}<br>Venue : ${data.venue}</p>
@@ -150,12 +150,12 @@ function getTeamById() {
                     <p>
                         <ul>
               `;
-              data.activeCompetitions.forEach(function(item) {
-              teamHTML += `
+                        data.activeCompetitions.forEach(function(item) {
+                            teamHTML += `
                         <li>${item.name}</li>
                           `;
-              });
-              teamHTML += `
+                        });
+                        teamHTML += `
                         </ul>
                       </p>
                     </div>
@@ -172,33 +172,33 @@ function getTeamById() {
                     </thead>
                     <tbody>
                           `;
-              data.squad.forEach(function(item) {
-              teamHTML += `
+                        data.squad.forEach(function(item) {
+                            teamHTML += `
                         <tr>
                         <td>${item.name}</td>
                         <td>${item.position}</td>
                         </tr>
                           `;
-              });
-              teamHTML += `
+                        });
+                        teamHTML += `
       
                   </div>
                 </div>
               </div>
                           `;
-            document.getElementById("body-content").innerHTML = teamHTML;
-          });
+                        document.getElementById("body-content").innerHTML = teamHTML;
+                    });
+                }
+            });
         }
-      });
-    }
-    fetchApi(team_id_url)
-      .then(status)
-      .then(json)
-      .then(function(data) {
-        // Objek/array JavaScript dari response.json() masuk lewat data.
-        console.log(data);
-        // tampilkan data detail team
-        var teamHTML = `
+        fetchApi(team_id_url)
+            .then(status)
+            .then(json)
+            .then(function(data) {
+                // Objek/array JavaScript dari response.json() masuk lewat data.
+                console.log(data);
+                // tampilkan data detail team
+                var teamHTML = `
         <div class="row">
           <h4 class="light center grey-text text-darken-3" style="font-size:40px; font-weight:bold;"><img style="width:90px;" src="${data.crestUrl}"> <br>${data.name}</h4>
                 <p align="center">Nickname : ${data.shortName}<br>Address : ${data.address}<br>Founded : ${data.founded}<br>Club Colors : ${data.clubColors}<br>Venue : ${data.venue}</p>
@@ -208,12 +208,12 @@ function getTeamById() {
                 <p>
                     <ul>
           `;
-          data.activeCompetitions.forEach(function(item) {
-          teamHTML += `
+                data.activeCompetitions.forEach(function(item) {
+                    teamHTML += `
                     <li>${item.name}</li>
                       `;
-          });
-          teamHTML += `
+                });
+                teamHTML += `
                     </ul>
                   </p>
                 </div>
@@ -230,20 +230,70 @@ function getTeamById() {
                 </thead>
                 <tbody>
                       `;
-          data.squad.forEach(function(item) {
-          teamHTML += `
+                data.squad.forEach(function(item) {
+                    teamHTML += `
                     <tr>
                     <td>${item.name}</td>
                     <td>${item.position}</td>
                     </tr>
                       `;
-          });
-          teamHTML += `
+                });
+                teamHTML += `
   
               </div>
             </div>
           </div>
                       `;
-        document.getElementById("body-content").innerHTML = teamHTML;
-      });
-  }
+                document.getElementById("body-content").innerHTML = teamHTML;
+                resolve(data);
+            });
+    })
+
+}
+
+function getSavedTeams() {
+    getALL().then(function(data) {
+        console.log(data);
+        //menyusun komponen card artikel secara dinamis
+        var teamsHTML = "";
+        data.forEach(function(data) {
+            teamsHTML += `
+      <div class="row">
+        <div class="col-s12">
+          <div class="card">
+            <div class="card-image">
+              <img src="${data.crestUrl}">
+              <button class="btn-floating halfway-fab waves-effect
+              waves-light red" id="delete" value="${data.id}"><i
+              class="material-icons">delete</i></button>
+            </div>
+            <div class="card-content">
+            <span class="card-title" align="center" style="font-weight:bold;
+            margin-bottom: 20px; color: #0D47A1;"><u>${data.name}</u></span>
+            <p align="center">Nickname : ${data.shortName}<br>Adress :
+            ${data.address}<br> Founded : ${data.founded}<br>Club Colors :
+            ${data.clubColors}<br>Venue : ${data.venue}</p>
+            </div>
+          </div>
+        </div>
+      </div>`;
+        });
+        //Sisipkan komponen card ke dalam elemen dengan id #body-content
+        document.getElementById("body-content").innerHTML = teamsHTML;
+        let btn = document.querySelectorAll(".btn-floating");
+        for (let button of btn) {
+            button.addEventListener("click", function(event) {
+                let id = Number(button.value);
+                console.log(id);
+                var toastHTML = `<span>Successfully remove the team from favorite</span>  <
+                    button onclick = "M.toast.getInstance(this.ParentElement).dismiss()"
+                class = "btn-flat toast - action " > Close < /button>`;
+                M.toast({ html: toastHTML });
+                dbDeleteTeam(id).then(() => {
+                    getSavedTeams()
+                })
+
+            })
+        }
+    });
+}
